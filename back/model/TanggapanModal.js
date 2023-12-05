d04efe8a196487632b998b6c39a2f2757384f5dc
AthaaFW <112294397+AthaaFW@users.noreply.github.com>
2023-12-05 09:17:04 +0700

    Add files via upload

---
+++ back/model/TanggapanModal.js
@@ -0,0 +1,28 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+import Pengaduan from "./PengaduanModal.js";
+import Petugas from "./PetugasModal.js";
+
+const {DataTypes} = Sequelize;
+
+const Tanggapan = db.define('tangappan',{
+    id_tanggapan:{
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    id_pengaduan: DataTypes.INTEGER,
+    tgl_tanggapan:{
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
+    tanggapan: DataTypes.TEXT,
+    id_petugas: DataTypes.INTEGER
+})
+
+
+export default Tanggapan;
+
+// (async()=>{
+//     await db.sync({alter: true});
+// })();
